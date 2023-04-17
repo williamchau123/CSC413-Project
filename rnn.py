2bd0070bcdafd7c39c8160334e1fa424caed8786
Robin-hhc <robinhuangqwe@gmail.com>
2023-04-17 14:29:23 -0400

    tune model and plot

--- rnn.py
+++ rnn.py
@@ -1,33 +1,51 @@
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification, TrainingArguments, Trainer
-from datasets import load_dataset
+from datasets import concatenate_datasets, load_dataset
+import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
 import evaluate
 
 df = load_dataset("csv", data_files="data.csv")
+df = df.remove_columns(['ID', 'Source'])
+redit = df["train"].select(range(125)).shuffle(seed=42)
+stack = df["train"].select(range(125, 250)).shuffle(seed=42)
+gpt_redit = df["train"].select(range(250, 375)).shuffle(seed=42)
+gpt_stack = df["train"].select(range(375, 500)).shuffle(seed=42)
+
+tokenizer = AutoTokenizer.from_pretrained("roberta-base-openai-detector")
+# for internet connection error, please download the model from https://huggingface.co/bert-base-cased and place the
+# folder in the same directory model = AutoModelForSequenceClassification.from_pretrained("./bert-base-cased",
+# num_labels=2)
+model = AutoModelForSequenceClassification.from_pretrained("roberta-base-openai-detector")
 
 
-tokenizer = AutoTokenizer.from_pretrained("bert-base-cased")
-# for internet connection error, please download the model from https://huggingface.co/bert-base-cased and place the folder in the same directory
-# model = AutoModelForSequenceClassification.from_pretrained("./bert-base-cased", num_labels=2)
-model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased", num_labels=2)
 def tokenize_function(examples):
     return tokenizer(examples["text"], padding="max_length", truncation=True)
 
-tokenized_datasets = df.map(tokenize_function, batched=True)
-train_set = tokenized_datasets['train'].select(range(200))
-val_set = tokenized_datasets['train'].select(range(200, 400))
-test_set = tokenized_datasets['train'].select(range(400, 500))
+
+tokenized_redit = redit.map(tokenize_function, batched=True)
+tokenized_stack = stack.map(tokenize_function, batched=True)
+tokenized_gpt_redit = gpt_redit.map(tokenize_function, batched=True)
+tokenized_gpt_stack = gpt_stack.map(tokenize_function, batched=True)
+train_set = concatenate_datasets([tokenized_redit.select(range(50)), tokenized_stack.select(range(50)),
+                                  tokenized_gpt_redit.select(range(50)), tokenized_gpt_stack.select(range(50))]).shuffle(seed=42)
+val_set = concatenate_datasets([tokenized_redit.select(range(50, 100)), tokenized_stack.select(range(50, 100)),
+                                tokenized_gpt_redit.select(range(50, 100)), tokenized_gpt_stack.select(range(50, 100))]).shuffle(seed=42)
+test_set = concatenate_datasets([tokenized_redit.select(range(100, 125)), tokenized_stack.select(range(100, 125)),
+                                 tokenized_gpt_redit.select(range(100, 125)), tokenized_gpt_stack.select(range(100, 125))]).shuffle(seed=42)
 
 args = TrainingArguments(
-    evaluation_strategy = "epoch",
-    save_strategy = "epoch",
-    learning_rate=2e-5,
-    num_train_epochs=5,
-    weight_decay=0.01,
+    evaluation_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=1e-8,
+    num_train_epochs=20,
+    weight_decay=0.001,
     load_best_model_at_end=True,
     output_dir="test_trainer",
-    metric_for_best_model="accuracy"
+    metric_for_best_model="accuracy",
+    report_to="none"
+    # resume_from_checkpoint=True
 )
 training_args = TrainingArguments(output_dir="test_trainer")
 
@@ -36,7 +54,7 @@ def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = np.argmax(logits, axis=-1)
     return metric.compute(predictions=predictions, references=labels)
-    
+
 trainer = Trainer(
     model=model,
     args=args,
@@ -44,7 +62,32 @@ trainer = Trainer(
     eval_dataset=val_set,
     compute_metrics=compute_metrics,
 )
+
+def plot():
+    print(trainer.state.log_history)
+    losses, iters, acc = [], [], []
+    for epoch in trainer.state.log_history[:-1]:
+        if 'eval_loss' in epoch:
+            losses.append(epoch['eval_loss'])
+            iters.append(epoch['epoch'])
+            acc.append(epoch['eval_accuracy'])
+    plt.title("Learning Curve")
+    plt.plot(iters, losses, label="Valid Loss")
+    plt.xlabel("Epochs")
+    plt.ylabel("Loss")
+    plt.show()
+
+    plt.title("Learning Curve")
+    plt.plot(iters, acc, label="Valid Accuracy")
+    plt.xlabel("Epochs")
+    plt.ylabel("Accuracy")
+    plt.show()
+
 train = trainer.train()
 evaluate = trainer.evaluate()
 print(train)
 print(evaluate)
+plot()
+pred = trainer.predict(test_set)
+print(pred)
+print(pred.metrics)
